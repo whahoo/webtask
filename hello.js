69a0d565f60427485ab83446b27b4602002218c9
whahoo <simon@evo2.com>
2017-06-01 13:19:39 +1000

    Update hello.js

--- hello.js
+++ hello.js
@@ -93,8 +93,8 @@ app.get("/pendingApprovals", jwtCheck, function(req,res,next) {
           var grantRequests = user.app_metadata.grantsRequests.filter( gr => apis.find( a => a.id == gr.api_id ) );
           return grantRequests.map( gr => {
             console.log(user.app_metadata);
-            var client_name = user.app_metadata.clients.find( client => { client.id == gr.client_id});
-            gr.client_name = client_name.name;
+            var client = user.app_metadata.clients.find( client => client.id == gr.client_id );
+            gr.client_name = client.name;
             gr.user_id = user.user_id;
             return gr; });
         });
